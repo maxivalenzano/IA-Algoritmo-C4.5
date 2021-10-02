fb4b50602da936433e0cfe1ff120111efc322d11
MaxiValenzano <maxivalenzano@gmail.com>
2021-10-02 19:29:34 -0300

    comentarios y mejoras

--- src/App.js
+++ src/App.js
@@ -3,37 +3,33 @@ import conjuntoEntrenamiento from './conjuntoEntrenamiento'
 import { cantidadApariciones, calculoEntropiaConjunto, listadoAtributos, nombreClase } from './funciones'
 
 const App = () => {
-const [umbral, setUmbral] = React.useState(0)
+  const [umbral, setUmbral] = React.useState(0)
 
-  //obtenemos la cantidad de veces que aparecen los valores de la clase en el conjunto de entrenamiento
-
+  // obtenemos el nombre de la clase
   const nombreDeClase = nombreClase(conjuntoEntrenamiento);
-
+  // obtenemos un listado de todos los componentes
   const listadoAtributoClases = listadoAtributos(conjuntoEntrenamiento, nombreDeClase);
-
+  // clasificamos en nombre y cantidad
   const cantidadAtributoClases = cantidadApariciones(listadoAtributoClases);
   console.log('🚀 ~ file: App.js ~ line 15 ~ App ~ cantidadAtributoClases', cantidadAtributoClases);
-
-  //se calcula la entropia del conjunto, para dos valores de una clase
-
-  const entropiaConjunto = calculoEntropiaConjunto(listadoAtributoClases, cantidadAtributoClases);
-
+  
+  //se calcula la entropia del conjunto para los valores de la clase
+  const entropiaConjunto = calculoEntropiaConjunto(listadoAtributoClases);
   console.log('entropia del Conjunto: ', entropiaConjunto);
-  console.log('🚀 ~ file: App.js ~ line 27 ~ App ~ umbral', umbral);
 
   return (
     <>
-    <p>Hello world!</p>
-    <p>Ingrese Umbral: </p>
-     <input
-      value={umbral}
-      onChange={e => setUmbral(e.target.value)}
-      type="number"
-      min={1}
-      max={100}
-     />
-     <p>Nombre de la clase: {nombreDeClase}</p>
-     <p>Entropia del conjunto: {entropiaConjunto}</p>
+      <p>Hello world!</p>
+      <p>Ingrese Umbral: </p>
+      <input
+        value={umbral}
+        onChange={e => setUmbral(e.target.value)}
+        type="number"
+        min={1}
+        max={100}
+      />
+      <p>Nombre de la clase: {nombreDeClase}</p>
+      <p>Entropia del conjunto: {entropiaConjunto}</p>
     </>
 
   );
--- src/funciones.js
+++ src/funciones.js
@@ -1,33 +1,44 @@
+
+// devuelve los titulos de la primer fila
 export const listadoTituloColumnas = (datos) => {
   return Object.keys(datos[0]);
 };
 
+// devuelve el nombre de la clase (ultima fila)
 export const nombreClase = (datos) => {
   const columnas = listadoTituloColumnas(datos)
-  return columnas[columnas.length-1]
+  return columnas[columnas.length - 1]
 };
 
-export const listadoAtributos = (datos, nombreCampo) => {
-  return datos.map(item => item[nombreCampo])
+// devuelve un listado con todos los campos de la fila dada
+export const listadoAtributos = (datos, nombreColumna) => {
+  return datos.map(item => item[nombreColumna])
 }
 
+// devuevle log en base 2
 export const log2 = (n) => {
-    return Math.log(n) / Math.log(2);
+  return Math.log(n) / Math.log(2);
 };
 
+// devuelve un objeto con cada nombre de atributo y su cantidad de aparicion
 export const cantidadApariciones = (arr) => {
-    const counts = {};
-    for (var i = 0; i < arr.length; i++) {
-       counts[arr[i]] = 1 + (counts[arr[i]] || 0);
-    };
-    return counts;
+  const counts = {};
+  for (var i = 0; i < arr.length; i++) {
+    counts[arr[i]] = 1 + (counts[arr[i]] || 0);
+  };
+  const object = Object.entries(counts).map(item => {
+    return { campo: item[0], cant: item[1] }
+  })
+  return object
 };
 
-export const calculoEntropiaConjunto = (listadoAtributos, attColumna) =>{
-    const cantAtrib = listadoAtributos.length;
-    let entropia = 0;
-    Object.values(attColumna).forEach(value => {
-      entropia = entropia + (-1*(value/cantAtrib)*(log2(value/cantAtrib)))
-    })
-    return entropia;
+// devuelve la entropia del conjunto
+export const calculoEntropiaConjunto = (columna) => {
+  const columnaClase = cantidadApariciones(columna)
+  const cantAtrib = columna.length;
+  let entropia = 0;
+  columnaClase.forEach((item) => {
+    entropia = entropia + (-1 * (item.cant / cantAtrib) * (log2(item.cant / cantAtrib)))
+  })
+  return entropia;
 };
