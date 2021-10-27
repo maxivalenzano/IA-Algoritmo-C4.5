b5b69cfc96eb4ba67153e1bb2c8e7d72ef689389
Otto Silva <ottosilva12@gmail.com>
2021-10-27 15:16:43 -0300

    aplicando recursividad, exitosa? tal vez

--- src/DecisionTree.jsx
+++ src/DecisionTree.jsx
@@ -11,73 +11,88 @@ import {
 } from './funciones';
 
 const DecisionTree = () => {
+
+
   const [umbral, setUmbral] = React.useState(0);
   // obtenemos el nombre de la clase
-  const clase = posicionClase(dataSet);
-  // obtenemos un listado de todos los componentes
-  const listadoValoresClases = listadoValoresColumna(dataSet, clase.nombre);
-  //se calcula la entropia del conjunto para los valores de la clase
-  const entropiaConjunto = calculoEntropíaConjunto(listadoValoresClases);
-  const entropiaTotalAtributos = calcularEntropiaTotalXAtributo(clase.nombre, dataSet);
-  const calculoGananciaInform = calculoGananciaInformacion(
-    entropiaTotalAtributos,
-    entropiaConjunto
-  );
-  const gananciaMaxima = maximoGanancia(calculoGananciaInform);
-  const dataSetForExpansion = filtradoSegunAtributoGananciaMaxima(gananciaMaxima, dataSet);
-  const nodo = {
-    nodo: gananciaMaxima,
-    dataSet: dataSetForExpansion,
-  };
-  console.log('🚀 ~ file: DecisionTree.jsx ~ line 32 ~ DecisionTree ~ primerNodo', nodo);
-  //segunda iteracion
-  const sexo = dataSetForExpansion.map((dataSetXValorAtributo) => {
-    //calculamos la entropia de los nuevos conjuntos de expansion
-    const listadoValoresClasesExpansion = listadoValoresColumna(
-      dataSetXValorAtributo.filas,
-      clase.nombre
+
+  const expansion = (dataSet) => {
+
+    if (dataSet.length === 0) { return console.log("nodo puro ashee") }
+    const clase = posicionClase(dataSet);
+    const listadoValoresClases = listadoValoresColumna(dataSet, clase.nombre);
+    const entropiaConjunto = calculoEntropíaConjunto(listadoValoresClases);
+    const entropiaTotalAtributos = calcularEntropiaTotalXAtributo(clase.nombre, dataSet);
+    const calculoGananciaInform = calculoGananciaInformacion(
+      entropiaTotalAtributos,
+      entropiaConjunto
     );
-    const entropiaConjuntoExpansion = calculoEntropíaConjunto(listadoValoresClasesExpansion);
-    // const entropiaAtributosIndividualesExpansion = (dataSetXValorAtributo.filas.length === 0) ? [] : calculoEntropiaIndividual(clase.nombre, dataSetXValorAtributo.filas);
-    const entropiaTotalAtributosExpansion =
-      dataSetXValorAtributo.filas.length === 0
-        ? []
-        : calcularEntropiaTotalXAtributo(clase.nombre, dataSetXValorAtributo.filas);
-    const gananciaInformacionExpansion =
-      dataSetXValorAtributo.filas.length === 0
-        ? []
-        : calculoGananciaInformacion(entropiaTotalAtributosExpansion, entropiaConjuntoExpansion);
-    const gananciaMaximaExpansion =
-      dataSetXValorAtributo.filas.length === 0 ? [] : maximoGanancia(gananciaInformacionExpansion);
-    const nuevoDataSetSinPurosSexo =
-      dataSetXValorAtributo.filas.length === 0
-        ? []
-        : filtradoSegunAtributoGananciaMaxima(gananciaMaximaExpansion, dataSetXValorAtributo.filas);
-    return {
-      valorAtributo: dataSetXValorAtributo.valorAtributo,
-      entropia: entropiaConjuntoExpansion,
-      entropiaAtributos: entropiaTotalAtributosExpansion,
-      gananciaInformacionExpansion,
-      gananciaMaximaExpansion,
-      nuevoDataSetSinPurosSexo,
+    const gananciaMaxima = maximoGanancia(calculoGananciaInform);
+    const dataSetForExpansion = filtradoSegunAtributoGananciaMaxima(gananciaMaxima, dataSet);
+    const nodo = {
+      nodo: gananciaMaxima,
+      dataSet: dataSetForExpansion,
     };
-  });
-  console.log('🚀 ~ file: DecisionTree.jsx ~ line 67 ~ sexo ~ sexo', sexo);
+    console.log('🚀 ~ file: DecisionTree.jsx ~ line 32 ~ DecisionTree ~ nodo', nodo);
+
+    const listadoDataSetsForExpansion = nodo.dataSet.map(filas => filas.filas);
+    console.log("🚀 ~ file: DecisionTree.jsx ~ line 38 ~ expansion ~ listadoDataSetsForExpansion", listadoDataSetsForExpansion)
+
+    listadoDataSetsForExpansion.map(dataSet => {
+      console.log("este es el nuevo dataSet", dataSet, "su longitud es: ", dataSet.length);
+      return expansion(dataSet)
+    })
+
+  }
+  expansion(dataSet);
 
   return (
     <React.Fragment>
-      <p>Ingrese Umbral: </p>
-      <input
-        value={umbral}
-        onChange={(e) => setUmbral(e.target.value)}
-        type="number"
-        min={1}
-        max={100}
-      />
-      <p>Nombre de la clase: {clase.nombre}</p>
-      <p>Entropía del conjunto: {entropiaConjunto}</p>
+      <p>hola mundo</p>
     </React.Fragment>
   );
 };
 
 export default DecisionTree;
+
+
+
+
+
+
+
+
+  // //segunda iteracion
+  // const sexo = dataSetForExpansion.map((dataSetXValorAtributo) => {
+  //   //calculamos la entropia de los nuevos conjuntos de expansion
+  //   const listadoValoresClasesExpansion = listadoValoresColumna(
+  //     dataSetXValorAtributo.filas,
+  //     clase.nombre
+  //   );
+  //   const entropiaConjuntoExpansion = calculoEntropíaConjunto(listadoValoresClasesExpansion);
+  //   // const entropiaAtributosIndividualesExpansion = (dataSetXValorAtributo.filas.length === 0) ? [] : calculoEntropiaIndividual(clase.nombre, dataSetXValorAtributo.filas);
+  //   const entropiaTotalAtributosExpansion =
+  //     dataSetXValorAtributo.filas.length === 0
+  //       ? []
+  //       : calcularEntropiaTotalXAtributo(clase.nombre, dataSetXValorAtributo.filas);
+  //   const gananciaInformacionExpansion =
+  //     dataSetXValorAtributo.filas.length === 0
+  //       ? []
+  //       : calculoGananciaInformacion(entropiaTotalAtributosExpansion, entropiaConjuntoExpansion);
+  //   const gananciaMaximaExpansion =
+  //     dataSetXValorAtributo.filas.length === 0 ? [] : maximoGanancia(gananciaInformacionExpansion);
+  //   const nuevoDataSetSinPurosSexo =
+  //     dataSetXValorAtributo.filas.length === 0
+  //       ? []
+  //       : filtradoSegunAtributoGananciaMaxima(gananciaMaximaExpansion, dataSetXValorAtributo.filas);
+  //   return {
+  //     valorAtributo: dataSetXValorAtributo.valorAtributo,
+  //     entropia: entropiaConjuntoExpansion,
+  //     entropiaAtributos: entropiaTotalAtributosExpansion,
+  //     gananciaInformacionExpansion,
+  //     gananciaMaximaExpansion,
+  //     nuevoDataSetSinPurosSexo,
+  //   };
+  // });
+  // console.log('🚀 ~ file: DecisionTree.jsx ~ line 67 ~ sexo ~ sexo', sexo);
+
