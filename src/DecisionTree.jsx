5753593c6600dcb79cc80799b48089273cbcff33
MaxiValenzano <maxivalenzano@gmail.com>
2021-10-27 20:29:02 -0300

    FuncionaRecursividadFullIIzquierda

--- src/DecisionTree.jsx
+++ src/DecisionTree.jsx
@@ -11,14 +11,13 @@ import {
 } from './funciones';
 
 const DecisionTree = () => {
-
-
   const [umbral, setUmbral] = React.useState(0);
   // obtenemos el nombre de la clase
 
   const expansion = (dataSet) => {
-
-    if (dataSet.length === 0) { return console.log("nodo puro ashee") }
+    if (dataSet.length === 0) {
+      return [];
+    }
     const clase = posicionClase(dataSet);
     const listadoValoresClases = listadoValoresColumna(dataSet, clase.nombre);
     const entropiaConjunto = calculoEntropíaConjunto(listadoValoresClases);
@@ -33,17 +32,22 @@ const DecisionTree = () => {
       nodo: gananciaMaxima,
       dataSet: dataSetForExpansion,
     };
-    console.log('🚀 ~ file: DecisionTree.jsx ~ line 32 ~ DecisionTree ~ nodo', nodo);
-
-    const listadoDataSetsForExpansion = nodo.dataSet.map(filas => filas.filas);
-    console.log("🚀 ~ file: DecisionTree.jsx ~ line 38 ~ expansion ~ listadoDataSetsForExpansion", listadoDataSetsForExpansion)
-
-    listadoDataSetsForExpansion.map(dataSet => {
-      console.log("este es el nuevo dataSet", dataSet, "su longitud es: ", dataSet.length);
-      return expansion(dataSet)
-    })
-
-  }
+    console.log('🚀 ~ file: DecisionTree.jsx ~ line 36 ~ expansion ~ nodo', nodo);
+    nodo.dataSet.map((rama) => {
+      if (rama.filas.length === 0) {
+        console.log(
+          'nodo: ',
+          nodo.nodo.atributo,
+          ', atributo tipo: ',
+          rama.valorAtributo,
+          ', es un nodo puro de clase: ',
+          rama.nodoPuro.campoClase
+        );
+        return [];
+      } else return expansion(rama.filas);
+    });
+  
+  };
   expansion(dataSet);
 
   return (
@@ -55,44 +59,36 @@ const DecisionTree = () => {
 
 export default DecisionTree;
 
-
-
-
-
-
-
-
-  // //segunda iteracion
-  // const sexo = dataSetForExpansion.map((dataSetXValorAtributo) => {
-  //   //calculamos la entropia de los nuevos conjuntos de expansion
-  //   const listadoValoresClasesExpansion = listadoValoresColumna(
-  //     dataSetXValorAtributo.filas,
-  //     clase.nombre
-  //   );
-  //   const entropiaConjuntoExpansion = calculoEntropíaConjunto(listadoValoresClasesExpansion);
-  //   // const entropiaAtributosIndividualesExpansion = (dataSetXValorAtributo.filas.length === 0) ? [] : calculoEntropiaIndividual(clase.nombre, dataSetXValorAtributo.filas);
-  //   const entropiaTotalAtributosExpansion =
-  //     dataSetXValorAtributo.filas.length === 0
-  //       ? []
-  //       : calcularEntropiaTotalXAtributo(clase.nombre, dataSetXValorAtributo.filas);
-  //   const gananciaInformacionExpansion =
-  //     dataSetXValorAtributo.filas.length === 0
-  //       ? []
-  //       : calculoGananciaInformacion(entropiaTotalAtributosExpansion, entropiaConjuntoExpansion);
-  //   const gananciaMaximaExpansion =
-  //     dataSetXValorAtributo.filas.length === 0 ? [] : maximoGanancia(gananciaInformacionExpansion);
-  //   const nuevoDataSetSinPurosSexo =
-  //     dataSetXValorAtributo.filas.length === 0
-  //       ? []
-  //       : filtradoSegunAtributoGananciaMaxima(gananciaMaximaExpansion, dataSetXValorAtributo.filas);
-  //   return {
-  //     valorAtributo: dataSetXValorAtributo.valorAtributo,
-  //     entropia: entropiaConjuntoExpansion,
-  //     entropiaAtributos: entropiaTotalAtributosExpansion,
-  //     gananciaInformacionExpansion,
-  //     gananciaMaximaExpansion,
-  //     nuevoDataSetSinPurosSexo,
-  //   };
-  // });
-  // console.log('🚀 ~ file: DecisionTree.jsx ~ line 67 ~ sexo ~ sexo', sexo);
-
+// //segunda iteracion
+// const sexo = dataSetForExpansion.map((dataSetXValorAtributo) => {
+//   //calculamos la entropia de los nuevos conjuntos de expansion
+//   const listadoValoresClasesExpansion = listadoValoresColumna(
+//     dataSetXValorAtributo.filas,
+//     clase.nombre
+//   );
+//   const entropiaConjuntoExpansion = calculoEntropíaConjunto(listadoValoresClasesExpansion);
+//   // const entropiaAtributosIndividualesExpansion = (dataSetXValorAtributo.filas.length === 0) ? [] : calculoEntropiaIndividual(clase.nombre, dataSetXValorAtributo.filas);
+//   const entropiaTotalAtributosExpansion =
+//     dataSetXValorAtributo.filas.length === 0
+//       ? []
+//       : calcularEntropiaTotalXAtributo(clase.nombre, dataSetXValorAtributo.filas);
+//   const gananciaInformacionExpansion =
+//     dataSetXValorAtributo.filas.length === 0
+//       ? []
+//       : calculoGananciaInformacion(entropiaTotalAtributosExpansion, entropiaConjuntoExpansion);
+//   const gananciaMaximaExpansion =
+//     dataSetXValorAtributo.filas.length === 0 ? [] : maximoGanancia(gananciaInformacionExpansion);
+//   const nuevoDataSetSinPurosSexo =
+//     dataSetXValorAtributo.filas.length === 0
+//       ? []
+//       : filtradoSegunAtributoGananciaMaxima(gananciaMaximaExpansion, dataSetXValorAtributo.filas);
+//   return {
+//     valorAtributo: dataSetXValorAtributo.valorAtributo,
+//     entropia: entropiaConjuntoExpansion,
+//     entropiaAtributos: entropiaTotalAtributosExpansion,
+//     gananciaInformacionExpansion,
+//     gananciaMaximaExpansion,
+//     nuevoDataSetSinPurosSexo,
+//   };
+// });
+// console.log('🚀 ~ file: DecisionTree.jsx ~ line 67 ~ sexo ~ sexo', sexo);
