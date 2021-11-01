b4f1e12b82c1897b1509047d5daf1b3ff5d68a2e
MaxiValenzano <maxivalenzano@gmail.com>
2021-10-31 21:57:17 -0300

    conjuntos

---
+++ src/conjuntoEntrenamiento4.js
@@ -0,0 +1,74 @@
+const conjuntoEntrenamiento3 = [
+    {
+        "ingreso": "medio",
+        "edad": ">35",
+        "cpa": "si",
+        "cs": "no",
+        "ag": "si"
+    },
+    {
+        "ingreso": "bajo",
+        "edad": ">35",
+        "cpa": "si",
+        "cs": "si",
+        "ag": "no"
+    },
+    {
+        "ingreso": "medio",
+        "edad": ">35",
+        "cpa": "si",
+        "cs": "no",
+        "ag": "no"
+    },
+    {
+        "ingreso": "medio",
+        "edad": ">35",
+        "cpa": "no",
+        "cs": "no",
+        "ag": "no"
+    },
+    {
+        "ingreso": "alto",
+        "edad": "<35",
+        "cpa": "si",
+        "cs": "no",
+        "ag": "no"
+    },
+    {
+        "ingreso": "alto",
+        "edad": "<35",
+        "cpa": "si",
+        "cs": "si",
+        "ag": "si"
+    },
+    {
+        "ingreso": "bajo",
+        "edad": ">35",
+        "cpa": "si",
+        "cs": "si",
+        "ag": "no"
+    },
+    {
+        "ingreso": "bajo",
+        "edad": "<35",
+        "cpa": "no",
+        "cs": "no",
+        "ag": "no"
+    },
+    {
+        "ingreso": "alto",
+        "edad": "<35",
+        "cpa": "si",
+        "cs": "no",
+        "ag": "no"
+    },
+    {
+        "ingreso": "alto",
+        "edad": ">35",
+        "cpa": "si",
+        "cs": "si",
+        "ag": "si"
+    }
+]
+
+  export default conjuntoEntrenamiento3;
